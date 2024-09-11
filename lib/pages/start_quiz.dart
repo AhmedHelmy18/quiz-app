6129c019c2753730ce2222380bd759f14594ee25
AhmedHelmy18 <ahmedmohammed18112003@gmail.com>
2024-09-11 23:32:30 +0300

    create start_quiz page

---
+++ lib/pages/start_quiz.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+import '../theme/theme.dart';
+import 'create_quiz.dart';
+import 'home.dart';
+
+class StartQuiz extends StatelessWidget {
+  const StartQuiz({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final deviceWidth = MediaQuery.of(context).size.width;
+    final deviceHeight = MediaQuery.of(context).size.height;
+
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          onPressed: () {
+            Navigator.pushReplacement(
+              context,
+              MaterialPageRoute(
+                builder: (context) => HomePage(),
+              ),
+            );
+          },
+          icon: Icon(
+            Icons.arrow_back,
+            color: Colors.white,
+          ),
+        ),
+        title: Text(
+          'Start Quiz',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: deviceWidth * 0.05,
+          ),
+        ),
+        backgroundColor: Color(textcolor),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'QUIZ',
+              style: TextStyle(
+                fontSize: deviceWidth * 0.15,
+              ),
+            ),
+            Text(
+              'TIME',
+              style: TextStyle(
+                fontSize: deviceWidth * 0.15,
+              ),
+            ),
+            SizedBox(
+              height: deviceHeight * 0.2,
+            ),
+            SizedBox(
+              width: deviceWidth * 0.7,
+              height: deviceHeight * 0.08,
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                    backgroundColor: Color(Buttoncolor)),
+                onPressed: () {
+                  Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => CreateQuiz(),
+                    ),
+                  );
+                },
+                child: Text(
+                  'Start Quiz',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: deviceWidth * 0.06,
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
