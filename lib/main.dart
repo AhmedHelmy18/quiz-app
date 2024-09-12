b0443cbe69d92efea6cea6d855db12f03617572c
AhmedHelmy18 <ahmedmohammed18112003@gmail.com>
2024-09-12 15:01:55 +0300

    add firebase to project

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:quiz_app/pages/signup.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
-  runApp(const QuizApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(QuizApp());
 }
 
 class QuizApp extends StatelessWidget {
